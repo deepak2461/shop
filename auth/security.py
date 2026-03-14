a1648f566182c43e53c6076c13bf2b627bee6eae
Anthony <anthonydeepak.ch1@gmail.com>
2026-03-14 17:20:38 +0530

    secutity.py file updated

---
+++ auth/security.py
@@ -0,0 +1,68 @@
+'''
+from fastapi import Depends, HTTPException
+from app.auth import get_current_user, require_admin
+
+@router.get("/api/products")
+def list_products(user=Depends(get_current_user)):
+    ...
+
+@router.post("/api/products")
+def create_product(user=Depends(require_admin)):
+    ...
+
+
+
+'''
+
+
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+SECRET_KEY = "DEEPAK"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")       #https://youtu.be/7t2alSnE2-I?si=c8Vu5jMVABHBxnXU&t=8890
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+    
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    return payload
+
+def require_admin(current_user=Depends(get_current_user)):
+    if current_user.get("role") != "admin":
+        raise HTTPException(status_code=403, detail = " Admin privileges required")
+    return current_user
